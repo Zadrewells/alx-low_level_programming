b85cdacbf1178a7d02f188b973088baf853bb267
Zadrewells <zadrewells@gmail>
2022-06-02 19:02:56 -0700

    6-sixe.c

---
+++ 0x00-hello_world/6-sixe.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char: %1 byte(s)\n"), Sizeof(char));
+	printf("Size of an int: %1 byte(s)\n"), Sizeof(int));
+	printf("Size of an long int: %1 byte(s)\n"), Sizeof(long int));
+	printf("Size of an long long int: %1 byte(s)\n"), Sizeof(long long int));
+	printf("Size of an float: %1 byte(s)\n"), Sizeof(float));
+        return (0);
+}
