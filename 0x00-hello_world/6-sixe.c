44ebb562eb4b8eaed42aba07d20d3a066ba791c2
Zadrewells <zadrewells@gmail>
2022-06-02 19:44:49 -0700

    6-sixe.c

--- 0x00-hello_world/6-sixe.c
+++ 0x00-hello_world/6-sixe.c
@@ -6,15 +6,16 @@
  */
 int main(void)
 {
-	char a;
+char a;
 	int b;
-	long int c;
-	long long int d;
-	float f;
+		long int c;
+			long long int d;
+					float f;
 	printf("Size of a char: %1 byte(s)\n"), Sizeof(char(a));
 	printf("Size of an int: %1 byte(s)\n"), Sizeof(int(b));
 	printf("Size of a long int: %1 byte(s)\n"), Sizeof(long int(c));
 	printf("Size of a long long int: %1 byte(s)\n"), Sizeof(long long int(d));
 	printf("Size of a float: %1 byte(s)\n"), Sizeof(float(f));
+
 	return (0);
 }
