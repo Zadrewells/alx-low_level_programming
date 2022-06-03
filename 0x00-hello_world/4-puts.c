a7f56ff786e002c195b8cb522e9c87665a731e28
Zadrewells <zadrewells@gmail>
2022-06-02 18:07:36 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 														{	puts(*\"Programming is like building a multilingual puzzle");		
- 						   											return (0);
+int main(void)
+{
+
+	char str[] = "Programming is like building a multilingual puzzle"
+
+	puts(str)
+
+	return (0);
+
 }
