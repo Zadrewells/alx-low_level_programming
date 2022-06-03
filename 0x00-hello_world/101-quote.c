3fb3e1938e8af9e7527ba9ef0d2cd1ba8c5b5a1d
Zadrewells <zadrewells@gmail>
2022-06-03 06:06:01 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,6 @@
 int main(void)
 {
 
-	fputs(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	puts(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (0);
-
 }
