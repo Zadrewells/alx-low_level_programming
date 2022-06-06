e5d8205cd5f7f0390f35283d23b930cc79bffe78
Zadrewells <zadrewells@gmail.com>
2022-06-06 08:18:58 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char x = 'z';
+
+	while (x >= 'a')
+	{
+	putchar(x);
+		x--;
+	}
+	putchar('\n');
+	return (0);
+}
