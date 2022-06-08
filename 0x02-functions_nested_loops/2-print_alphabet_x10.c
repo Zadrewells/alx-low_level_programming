e6b10974a781675f8801caeff778b83937454db1
Zadrewells <zadrewells@gmail.com>
2022-06-08 15:58:47 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,18 +7,15 @@
  */
 void print_alphabet_x10(void)
 {
-	char c;
+	int c;
 	int i = 0;
 
-	while (i <= 9)
+	for (i = 0; i < 10, i++)
 	{
 		for (c = "a"; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
-
 		_putchar('\n');
-
-		i++;
 	}
 }
