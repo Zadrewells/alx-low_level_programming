e934d3538e929ee8287942efcbd673d47d0edf21
Zadrewells <zadrewells@gmail.com>
2022-06-08 16:26:28 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ *
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false.
+ */
+int _islower(int c)
+{
+	{
+		return (c >= 'a' && c <= 'z');
+	}
+}
