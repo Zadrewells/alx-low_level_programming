fa68e4f43168085fffbeae322242a6861056d0d3
Zadrewells <zadrewells@gmail.com>
2022-06-09 04:27:00 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,14 +13,13 @@ int main(void)
 
 	i = 0;
 
-	/*while (i < 10)*/
-	/*{*/
-
-		/*putchar(i);*/
-
-	/*}*/
-
-	printf("Infinite loop avoided! \\o/\n");
+	/**
+	 * while (i < 10)
+	 * {
+	 *putchar(i);
+	 *}
+	 */
+	printf("%d is the largest number\n", largest);
 
 	return (0);
 }
