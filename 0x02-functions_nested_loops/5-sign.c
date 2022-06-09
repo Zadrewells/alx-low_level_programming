c6a4d890932c9e42ae8bc2da1a37209464bce105
Zadrewells <zadrewells@gmail.com>
2022-06-08 17:53:15 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Function prototype
+ *
+ * @n: the number to be checked
+ * Return: 1 if positive, 0 if equals to 0 otherwise -1
+ */
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('_');
+		return (-1);
+	}
+
+}
