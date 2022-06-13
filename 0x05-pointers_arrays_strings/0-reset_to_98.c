90945679313da77fb278ebd91076ff4ca3f77106
Zadrewells <zadrewells@gmail.com>
2022-06-13 10:06:24 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_t0_98 - function prototype
+ * Description: Given a pointer to an int, reset the value of the int to 98
+ * @n; a pointer to an integer
+ * Return: void
+ */
+
+void reset_t0_98(int *n)
+{
+*n = 98;
+}
