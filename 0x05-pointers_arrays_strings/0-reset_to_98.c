b8b134f9da17209e73dd3b0c3c626c1b7d432dc5
Zadrewells <zadrewells@gmail.com>
2022-06-13 10:16:42 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_t0_98 - function prototype
  * Description: Given a pointer to an int, reset the value of the int to 98
- * @n; a pointer to an integer
+ * @n: a pointer to an integer
  * Return: void
  */
 
