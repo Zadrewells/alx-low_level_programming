19caa32f02d3eb1d723140e65f90ac2e6d7aa355
Zadrewells <zadrewells@gmail.com>
2022-06-16 17:03:11 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,19 +10,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
+	int i;
 
-	if (*s1 == *s2)
+	for (i = 0; s1[i] || s2[i]; i++)
 	{
-		return (0);
+		if (s1[i] != s2[i])
+		return (s1[i] - s2[i]);
 	}
-
-	else
-	{
-	return (*s1 - *s2);
-	}
+return (0);
 }
