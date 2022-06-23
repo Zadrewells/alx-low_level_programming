db957f464c6ec559ec635f9f0e87f8a14474bcea
Zadrewells <zadrewells@gmail.com>
2022-06-22 17:06:32 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#inculde "main.h"
+
+/**
+ * wildcmp - function prototype
+ * Description: compares two strings
+ * @s1: pointer to the first string to compare
+ * @s2: pointer to the second string to compare
+ * Return: 1 if the strings are considered identical otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s == '*')
+			return (wildcmp(s1, s2 + 1));
+
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+
+	return (*s1 == *s2 ? wildcmp(s1 + 1, s2 + 1) : 0);
+}
