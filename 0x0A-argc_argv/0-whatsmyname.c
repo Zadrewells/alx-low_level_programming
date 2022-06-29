93410c03c58e156232f3c90c6baa8291e6715532
Zadrewells <zadrewells@gmail.com>
2022-06-29 15:46:08 -0700

    printing name of the file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function prototype
+ * Description: prints the program name, followed by a new line
+ * @agrc: the number of arguments supplied to the program
+ * @argv: an array of pointers to arguement strings
+ * Return: Always 0
+ */
+int main(int argc; char *argv[])
+{
+	printf("%s\n", *argv[0]);
+
+	return (0);
+}
