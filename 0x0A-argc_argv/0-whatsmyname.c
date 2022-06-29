3d250e962b8261533b54a6280683933a4fcd4c94
Zadrewells <zadrewells@gmail.com>
2022-06-29 16:15:33 -0700

    fix it

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,12 +5,12 @@
  * Description: prints the program name, followed by a new line
  * @agrc: argc parameter
  * @argv: an array of a command listed
+ *
  * Return: 0 for success
  */
 
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int _attribute_((_unused_)) argc, char const *argv[])
 {
 	printf("%s\n", *argv[0]);
-
 	return (0);
 }
